0fc043c68454314a2e4904d5ab4c64a40fbf729d
jelly <jamesla200210@gmail.com>
2024-03-22 14:21:37 +1300

    Changed ports

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.use("^/$", (req, res, next) => {
   fs.readFile(path.resolve("./build/index.html"), "utf-8", (err, data) => {
     if (err) {
